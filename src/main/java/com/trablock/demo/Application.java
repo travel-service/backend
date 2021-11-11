df159ce2d83d749c2b820262a2a0d730d8e055da
wkd-woo <wkdwoos@gmail.com>
2021-11-11 17:00:18 +0900

    init

--- src/main/java/com/trablock/demo/Application.java
+++ src/main/java/com/trablock/demo/Application.java
@@ -2,18 +2,15 @@ package com.trablock.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@ComponentScan(basePackages = {"com.trablock.demo.controller"})
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @GetMapping("/")
-    @ResponseBody
-    public String home() {
-        return "Hello world";
-    }
 }
