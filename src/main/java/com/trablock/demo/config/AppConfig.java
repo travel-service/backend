3075514ff7db5262b19011528da0e1b7bfcb541b
wkd-woo <wkdwoos@gmail.com>
2021-11-24 14:48:44 +0900

    Feat: SpringDataJPA - AppConfig에 @EnableJPARepositories 추가

--- src/main/java/com/trablock/demo/config/AppConfig.java
+++ src/main/java/com/trablock/demo/config/AppConfig.java
@@ -2,12 +2,14 @@ package com.trablock.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Password encoder Bean 주입
  * */
 @Configuration
+@EnableJpaRepositories(basePackages = "com.trablock.demo.repository")
 public class AppConfig {
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
