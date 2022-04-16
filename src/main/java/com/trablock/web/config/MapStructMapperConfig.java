14ee973ac1de71d9cb16797ee5b4aaae05180895
wkd-woo <wkdwoos@gmail.com>
2022-04-16 16:47:02 +0900

    config(MapStructMapperConfig): Mapper 설정 공통화 - MapStructMapperConfig

---
+++ src/main/java/com/trablock/web/config/MapStructMapperConfig.java
@@ -0,0 +1,10 @@
+package com.trablock.web.config;
+
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR, uses = JsonMapper.class)
+public class MapStructMapperConfig {
+
+}
