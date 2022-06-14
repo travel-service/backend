7aa33625af8141d4b85ababbcb5841ee9c5b7d3a
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-06-14 17:02:10 +0900

    Refactor: ConceptRepository 필요 없는 query문 삭제

--- src/main/java/com/trablock/web/repository/plan/ConceptRepository.java
+++ src/main/java/com/trablock/web/repository/plan/ConceptRepository.java
@@ -1,6 +1,5 @@
 package com.trablock.web.repository.plan;
 
-import com.trablock.web.dto.plan.ConceptDto;
 import com.trablock.web.entity.plan.Concept;
 import com.trablock.web.entity.plan.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,9 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface ConceptRepository extends JpaRepository<Concept, Long> {
-
-    @Query("select c from Concept as c where c.id= :id")
-    Concept findConceptById(Long conceptId);
 
     @Query("select c.conceptName from Concept c where c.plan = :id")
     List<String> findByIdToList(@Param("id") Plan id);
