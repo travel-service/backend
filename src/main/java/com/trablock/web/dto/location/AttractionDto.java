b8aeac7e4b036d42cd6410483e234edeb82baa74
wkd-woo <wkdwoos@gmail.com>
2022-06-23 17:51:17 +0900

    refacor(Location): type dto 변경. /get api 전면 변경 예정

--- src/main/java/com/trablock/web/dto/location/AttractionDto.java
+++ src/main/java/com/trablock/web/dto/location/AttractionDto.java
@@ -1,5 +1,6 @@
 package com.trablock.web.dto.location;
 
+import com.trablock.web.domain.LocationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,7 +13,17 @@ import static lombok.AccessLevel.PROTECTED;
 @AllArgsConstructor
 @NoArgsConstructor(access = PROTECTED)
 public class AttractionDto {
-    
+
+    private Long locationId;
+    private String name;
+    private String address1;
+    private String address2;
+    private String image;
+    private String tel;
+    private String summary;
+    private String report;
+    private LocationType type;
+
     private boolean parking;
     private String restDate;
 
