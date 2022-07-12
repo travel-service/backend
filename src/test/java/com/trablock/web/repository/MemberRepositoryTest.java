9de36a92f10c2d57abc2ff64f4a1ac6aec1d1be5
wkd-woo <wkdwoos@gmail.com>
2022-07-13 00:55:53 +0900

    feat(LocationRepository): findAllByLocationIdIn()) : Id 리스트로 조회
    Id 리스트로 location 조회하는 repository 메소드 추가

--- src/test/java/com/trablock/web/repository/MemberRepositoryTest.java
+++ src/test/java/com/trablock/web/repository/MemberRepositoryTest.java
@@ -1,7 +1,6 @@
 package com.trablock.web.repository;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 
